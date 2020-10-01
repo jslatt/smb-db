3e9eff0f5f5f4d7ff82f4a1903df8e67533ece86
James <james@blueve.in>
2020-10-01 14:02:22 -0700

    Fix

--- server/main.js
+++ server/main.js
@@ -12,52 +12,62 @@ Meteor.publish('Watchlist', function () {
 
 Meteor.methods({
   'stockScrape'({stock, id}) {
-    const stockURL = 'https://finviz.com/quote.ashx?t=' + stock;
+      const stockURL = 'https://finviz.com/quote.ashx?t=' + stock;
 
-    let stats;
+      let stats = {};
 
-    request(stockURL, (error, response, html) => {
-      if(!error && response.statusCode == 200) {
-          const $ = cheerio.load(html);
-  
-          const dataTable = $('.snapshot-table2').text();
-          
-          // ATR
-          stringSearcher.find(dataTable,'ATR')
-              .then(function(resultArr){
-                  let fatr = resultArr[0].text;
-                  stats = fatr;
-              })
-              stringSearcher.find(dataTable,'Avg Volume')
-              .then(function(resultArr){
-                  let favol = resultArr[0].text;
-
-              })
-              stringSearcher.find(dataTable,'Rel Volume')
-              .then(function(resultArr){
-                  let frvol = resultArr[0].text;
-     
-              })
-              stringSearcher.find(dataTable,'Shs Float')
-              .then(function(resultArr){
-                  let ffloat = resultArr[0].text;
+      request(stockURL, (error, response, html) => {
+        if(!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+    
+            const dataTable = $('.snapshot-table2').text();
             
-              })
-              stringSearcher.find(dataTable,'Short Float')
-              .then(function(resultArr){
-                  let fsfloat = resultArr[0].text;
-                  stats[4] = fsfloat;
-
-              })
-              stringSearcher.find(dataTable,'Inst Own')
-              .then(function(resultArr){
-                  let finst = resultArr[0].text;
-        
-              })
-            }
-  });
-  // Add update 
+            // ATR
+            stringSearcher.find(dataTable,'ATR')
+                .then(function(resultArr){
+                    let fatr = resultArr[0].text.substring(3);
+                })
+                stringSearcher.find(dataTable,'Avg Volume')
+                .then(function(resultArr){
+                    let favol = resultArr[0].text.substring(10);
+  
+                })
+                stringSearcher.find(dataTable,'Rel Volume')
+                .then(function(resultArr){
+                    let frvol = resultArr[0].text.substring(10);
+       
+                })
+                stringSearcher.find(dataTable,'Shs Float')
+                .then(function(resultArr){
+                    let ffloat = resultArr[0].text.substring(9);
+              
+                })
+                stringSearcher.find(dataTable,'Short Float')
+                .then(function(resultArr){
+                    let fsfloat = resultArr[0].text.substring(11);
+                    stats[4] = fsfloat;
+  
+                })
+                stringSearcher.find(dataTable,'Inst Own')
+                .then(function(resultArr){
+                    let finst = resultArr[0].text.substring(8);
+  
+                })
+              }
+    });
+ 
+  stats = {
+    atr: fatr,
+    avol: favol,
+    rvol: frvol,
+    float: ffloat,
+    sfloat: fsfloat,
+    inst: finst
+  }
   console.log(stats);
+  //Watchlist.update(id,{ $set: {atr: fatr}});
+
+
   }
 });
 
